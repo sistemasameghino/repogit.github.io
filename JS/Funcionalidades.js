afb362a23d8e470a0fb4e40b494f7defe8efbe00
Departamento de Sistemas <sistemas@AMEGHINO.local>
2023-03-17 14:17:29 -0300

    first commit

---
+++ JS/Funcionalidades.js
@@ -0,0 +1,22 @@
+
+document.getElementById('educacion').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#F69F2C';
+});
+document.getElementById('perfil').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#F69F2C';
+});
+document.getElementById('datos').addEventListener('click', function () {
+    document.getElementsByClassName('iconos').style.color ='#F69F2C' ;
+});
+document.getElementById('experiencia').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#F69F2C';
+});
